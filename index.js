55365a1b004f4b57fc3a97763858ca0597365c4a
Rahul Paswan <rahulpaswandoon@gmail.com>
2024-10-13 19:39:07 +0530

    first commit

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
-require("dotenv").config()
+require("dotenv").config(); // Load environment variables from .env file
 
-const express = require("express")
-const {connectMongoDB} = require("./connection")
-const userRoute = require("./routes/user")
-const bodyParsser = require("body-parser")
-const bodyParser = require("body-parser")
-const quizRoute = require("./routes/quiz")
-const cookieParser = require("cookie-parser")
-const {checkForAuthenticateUser} = require("./middlewares/authentication")
+const express = require("express");
+const { connectMongoDB } = require("./connection");
+const userRoute = require("./routes/user");
+const quizRoute = require("./routes/quiz");
+const cookieParser = require("cookie-parser");
+const { checkForAuthenticateUser } = require("./middlewares/authentication");
+const bodyParser = require("body-parser");
 
-const app = express()
-const PORT = process.env.PORT || 8000
+const app = express();
+const PORT = process.env.PORT || 8000; // Set the port to the environment variable or default to 8000
 
+// Connect to MongoDB database using URL from .env file
 connectMongoDB(process.env.MONGO_URL)
-.then(()=> console.log("MongoDB is conncted successfully !!"))
-.catch((error) => console.log("There is some error while connecting !!"))
+    .then(() => console.log("MongoDB is connected successfully !!"))
+    .catch((error) => console.log("There is some error while connecting !!"));
 
-app.get("/",(req,res)=>{
-     return res.send("From the server")
-})
+// Basic home route to check if server is running
+app.get("/", (req, res) => {
+    return res.send("From the server");
+});
 
-app.use(bodyParser.json())
-app.use(cookieParser())
-app.use(checkForAuthenticateUser("token"))
+// Middleware to parse incoming JSON payloads
+app.use(bodyParser.json());
 
-app.use("/api/user",userRoute)
-app.use("/api/quiz",quizRoute)
+// Middleware to parse cookies
+app.use(cookieParser());
 
-app.listen(PORT,()=>{
-    console.log("Server is runing at port 8000")
-})
+// Middleware to check for authentication on protected routes
+app.use(checkForAuthenticateUser("token"));
+
+// Routes for user registration and login
+app.use("/api/user", userRoute);
+
+// Routes for quiz-related operations
+app.use("/api/quiz", quizRoute);
+
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+});
